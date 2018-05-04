1352dd2eff618a210a186321b862d79cebc15a57
arceus6666 <arceus6666@hotmail.com>
2018-05-04 07:57:08 -0400

    pares primos fibonacci return int[]

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -6,25 +6,37 @@ import java.util.Arrays;
 public class Main2 {
 
     public static void main(String[] args) {
-        int n = 11;
-        // TODO Auto-generated method stub
+
+        final int N = 11;
         System.out.println("Pares");
-        pares(n);
+        System.out.print(Arrays.toString(getNPairs(N)));
         System.out.println("\nFibonacci");
-        fibonacci(n);
+        System.out.print(Arrays.toString(getNFibonacci(N)));
         System.out.println("\nPrimos");
-        primos(n);
+        System.out.print(Arrays.toString(getNPrimes(N)));
 
     }
 
-    static void pares(int n) {
-        for (int i = 0, j = 2; i < n; i++, j += 2) {
-            System.out.print(j + ", ");
+    /**
+     *
+     * @param n
+     * @return
+     */
+    static int[] getNPairs(int n) {
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i]=i*2;
         }
+        return res;
     }
 
-    static int[] fibo2(int n) {
-        // System.out.println("n" + n);
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    static int[] getNFibonacci(int n) {
         int[] memo = new int[n];
         memo[0] = 1;
         memo[1] = 1;
@@ -34,24 +46,27 @@ public class Main2 {
         return memo;
     }
 
-    static void fibonacci(int n) {
-        System.out.print(Arrays.toString(fibo2(n)));
-    }
-
-    static void primos(int n) {
-
-        for (int j = 2, c2 = 0; j < Integer.MAX_VALUE && c2 < n; j++) {
-            int c = 0;
+    /**
+     *
+     * @param n
+     * @return
+     */
+    static int[] getNPrimes(int n) {
+        int[] res= new int[n];
+        for (int j = 2, count2 = 0; j < Integer.MAX_VALUE && count2 < n; j++) {
+            int count = 0;
             for (int k = 2; k <= j; k++) {
                 if (j % k == 0)
-                    c++;
+                    count++;
             }
-            if (c == 1) {
-                System.out.print(j + ", ");
-                c2++;
+            if (count == 1) {
+                //System.out.print(j + ", ");
+                res[count2] = j;
+                count2++;
             }
 
         }
+        return res;
     }
 
 }
